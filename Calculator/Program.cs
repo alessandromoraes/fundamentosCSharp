3c3158288d8fd9d469f08db892a9c14581e88b20
Alessandro Moraes <alessandro.moraes07@gmail.com>
2021-07-04 19:36:11 -0300

    Commit inicial

---
+++ Calculator/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Calculator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // Menu
+         Menu();
+         Console.ReadKey();
+      }
+
+      static void Soma()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor:");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor:");
+         float v2 = float.Parse(Console.ReadLine());
+
+         var resultado = v1 + v2;
+
+         Console.WriteLine("");
+         Console.WriteLine($"O resultado da soma é: {resultado}");
+      }
+
+      static void Subtracao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor:");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor:");
+         float v2 = float.Parse(Console.ReadLine());
+
+         var resultado = v1 - v2;
+
+         Console.WriteLine("");
+         Console.WriteLine($"O resultado da subtração é: {resultado}");
+      }
+
+      static void Divisao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor:");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor:");
+         float v2 = float.Parse(Console.ReadLine());
+
+         var resultado = v1 / v2;
+
+         Console.WriteLine("");
+         Console.WriteLine($"O resultado da divisão é: {resultado}");
+      }
+
+      static void Multiplicacao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor:");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor:");
+         float v2 = float.Parse(Console.ReadLine());
+
+         var resultado = v1 * v2;
+
+         Console.WriteLine("");
+         Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+      }
+
+      static void Menu()
+      {
+         Console.WriteLine("O que deseja fazer?");
+         Console.WriteLine("1 - Soma");
+         Console.WriteLine("2 - Subtração");
+         Console.WriteLine("3 - Divisão");
+         Console.WriteLine("4 - Multiplicação");
+         Console.WriteLine("5 - Sair");
+         Console.WriteLine("-------------------");
+         var menu = int.Parse(Console.ReadLine());
+         switch (menu)
+         {
+            case 1: Soma(); break;
+            case 2: Subtracao(); break;
+            case 3: Divisao(); break;
+            case 4: Multiplicacao(); break;
+            case 5: System.Environment.Exit(0); break;
+            default: Menu(); break;
+         }
+         Menu();
+      }
+   }
+}
